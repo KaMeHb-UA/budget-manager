b9d84ec6bfb9a8f98a1293daed7681d234a04b66
KaMeHb-UA <marlock@bassteam.ml>
2021-08-18 20:39:16 +0300

    callHooks

--- app/components/core.js
+++ app/components/core.js
@@ -1,3 +1,6 @@
+import { bold } from '../helpers/os/output.js';
+import request from './jsonrpc.js';
+
 export const hooks = (
     /**
      * @template {string} T
@@ -14,6 +17,23 @@ export const hooks = (
     'balance',
     'transactions',
 );
+
+/**
+ * @arg {keyof hooks} name
+ * @arg {any} args
+ * @return {Promise<[string, any][]>}
+ */
+async function callHooks(name, args){
+    // @ts-ignore
+    return Promise.all(hooks[name].map(async ([plugin, url]) => {
+        try{
+            return [plugin, await request(url, name, args)];
+        } catch(e){
+            console.error(`[${bold(plugin)}]: failed to call ${name}: ${e.message}`);
+            return [plugin, undefined];
+        }
+    }));
+}
 
 export default function(){
 }
