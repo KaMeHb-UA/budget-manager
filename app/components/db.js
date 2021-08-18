35c9dafd42d5d10140829a0db63ac23594d227ee
KaMeHb-UA <marlock@bassteam.ml>
2021-08-19 01:03:56 +0300

    db component

---
+++ app/components/db.js
@@ -0,0 +1,38 @@
+import callHooks from './hooks.js';
+
+
+/**
+ * @arg {`db_${'get' | 'add' | 'update'}`} hook
+ * @arg {any} data
+ * @return {Promise<any>}
+ */
+async function call(hook, data){
+    const v = await callHooks(hook, data);
+    return v[Object.keys(v)[0]];
+}
+
+/**
+ * @arg {any} options
+ * @return {Promise<{id: string, data: any}[]>}
+ */
+export async function get(options){
+    return call('db_get', options);
+}
+
+/**
+ * @arg {string} table
+ * @arg {any} data
+ * @return {Promise<string>}
+ */
+export async function add(table, data){
+    return call('db_add', { table, data });
+}
+
+/**
+ * @arg {string} table
+ * @arg {string} id
+ * @arg {any} data
+ */
+export async function update(table, id, data){
+    await call('db_update', { table, id, data });
+}
