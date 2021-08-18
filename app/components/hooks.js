6cc2efcebc863b6e8676c1ed9501f62877ab0787
KaMeHb-UA <marlock@bassteam.ml>
2021-08-19 00:26:03 +0300

    move callHooks to hooks component

--- app/components/hooks.js
+++ app/components/hooks.js
@@ -1,4 +1,7 @@
-export default (
+import { bold } from '../helpers/os/output.js';
+import request from './jsonrpc.js';
+
+const hooks = (
     /**
      * @template {string} T
      * @param  {...T} hooks
@@ -20,3 +23,21 @@ export default (
     'db_add',
     'db_update',
 );
+
+/**
+ * @arg {keyof hooks} name
+ * @arg {any} args
+ * @return {Promise<{[x: string]: any}>}
+ */
+export default async function(name, args){
+    const res = {};
+    for(const [ plugin, result ] of await Promise.all(hooks[name].map(async ([plugin, url]) => {
+        try{
+            return [plugin, await request(url, name, args)];
+        } catch(e){
+            console.error(`[${bold(plugin)}]: failed to call ${name}: ${e.message}`);
+            return [plugin, undefined];
+        }
+    }))) res[plugin] = result;
+    return res;
+}
