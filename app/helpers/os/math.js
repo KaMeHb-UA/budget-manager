0d1e536ea579e4b98f777bf5b3beb49485ba1ad6
KaMeHb-UA <marlock@bassteam.ml>
2021-08-18 23:30:16 +0300

    math with high precision

---
+++ app/helpers/os/math.js
@@ -0,0 +1,37 @@
+/*!
+ * Fixed math module
+ */
+
+/**
+ * @arg {number} num
+ */
+function digitsAfterDot(num){
+    return ((num + '').split('.')[1] || '').length;
+}
+
+/**
+ * @arg {number} op1
+ * @arg {number} op2
+ * @arg {(op1: number, op2: number, precision?: number) => number} cb
+ */
+function baseOp(op1, op2, cb){
+    const dad1 = digitsAfterDot(op1), dad2 = digitsAfterDot(op2);
+    const targetPrecision = 10 ** dad1 > dad2 ? dad1 : dad2;
+    return cb(op1 * targetPrecision, op2 * targetPrecision, targetPrecision) / targetPrecision;
+}
+
+/**
+ * @arg {number} op1
+ * @arg {number} op2
+ */
+export function minus(op1, op2){
+    return baseOp(op1, op2, (op1, op2) => op1 - op2)
+}
+
+/**
+ * @arg {number} op1
+ * @arg {number} op2
+ */
+export function plus(op1, op2){
+    return baseOp(op1, op2, (op1, op2) => op1 + op2)
+}
