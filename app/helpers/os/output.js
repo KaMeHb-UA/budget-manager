43bd26eb2c0d2c90384969d0e10325feb67147fa
KaMeHb-UA <marlock@bassteam.ml>
2021-08-19 02:15:38 +0300

    prettify output

--- app/helpers/os/output.js
+++ app/helpers/os/output.js
@@ -2,5 +2,12 @@
  * @arg {string} str
  */
 export function bold(str){
-    return `${str}`;
+    return `\u001b[1m${str}\u001b[0m`;
+}
+
+/**
+ * @arg {string} str
+ */
+export function red(str){
+    return `\u001b[31;1m${str}\u001b[0m`;
 }
--- app/index.js
+++ app/index.js
@@ -1,15 +1,27 @@
 import registerPlugin, { isDBRegistered } from './components/plugin-loader.js';
 import { plugins } from './helpers/os/args.js';
-import { bold } from './helpers/os/output.js';
+import { bold, red } from './helpers/os/output.js';
 import run from './components/core.js';
+import { exit } from 'process';
 
 await Promise.all(plugins.map(async url => {
-    console.log(`Registering plugin at ${url}...`);
-    const { name, hooks } = await registerPlugin(url);
-    if(hooks.length) console.log(`[${bold(name)}]: plugin registered successfully with hooks ${hooks.join(', ')}`);
-    else console.error(`[${bold(name)}]: error: cannot register plugin: there is no compatible hooks`);
+    try{
+        console.log(`[${red('core')}]: registering plugin from ${url}...`);
+        const { name, hooks } = await registerPlugin(url);
+        if(hooks.length) console.log(`[${bold(name)}]: plugin registered successfully with hooks ${hooks.join(', ')}`);
+        else {
+            console.error(`[${bold(name)}]: error: cannot register plugin: there is no compatible hooks`);
+            exit(1);
+        }
+    } catch(e){
+        console.error(`[${red('core')}]: error: cannot register plugin from ${url}: ${e.message}`);
+        exit(1);
+    }
 }));
 
-if(!isDBRegistered()) throw new Error('Cannot start core without DB plugin. Please, add one');
+if(!isDBRegistered()){
+    console.error(`[${red('core')}]: error: start core without DB plugin. Please, add one`);
+    exit(1);
+}
 
 void run();
