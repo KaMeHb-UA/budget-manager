390bcf2fbece1071f13fdbaa718b3d97bb5b72b8
KaMeHb-UA <marlock@bassteam.ml>
2021-08-19 03:16:47 +0300

    update checks in checkForNewTxs

--- app/components/txs.js
+++ app/components/txs.js
@@ -1,28 +1,44 @@
 import { get } from './db.js';
 import callHooks from './hooks.js';
 import { minus } from '../helpers/os/math.js';
+import { isBalanceData } from './types';
+import { bold } from '../helpers/os/output.js';
+
+/** @typedef {import('./types').Tx} Tx */
+/** @typedef {import('./types').BalanceData} BalanceData */
 
 /**
- * @typedef BalanceData
- * @property {string} provider
- * @property {{[x: string]: number}} balances
+ * @return {Promise<Tx[]>}
  */
-
 export async function checkForNewTxs(){
     // Changed balance is the reason to check for new txs.
     // Unlike crypto, some banks can hide some txs, but balance will change.
     // In that case we can assume that we have some hidden transaction(s?)
-    /** @type {[{id: string, data: BalanceData}[], {[provider: string]: BalanceData['balances']}]} */
     const [ dbBalances, providerBalances ] = await Promise.all([
         get({
             table: 'balances',
         }),
         callHooks('balance'),
     ]);
+    if('error' in dbBalances){
+        console.error(`[${bold(dbBalances.provider)}]: error: ${dbBalances.error}`)
+        return [];
+    }
     const changes = [];
     for(const provider in providerBalances){
         const currentBalances = providerBalances[provider];
-        const savedBalances = dbBalances.filter(({data}) => data.provider === provider)[0];
+        if(!isBalanceData({
+            provider,
+            balances: currentBalances,
+        })){
+            console.log(`[${bold(provider)}]: error: balance data returned in unknown format`);
+            continue;
+        }
+        const savedBalances = dbBalances.data.filter(({data}) => data?.provider === provider)[0];
+        if(savedBalances && !isBalanceData(savedBalances.data)){
+            console.log(`[${bold(dbBalances.provider)}]: error: balance data for provider '${provider}' returned in unknown format`);
+            continue;
+        }
         for(const currency in currentBalances){
             const currentBalance = currentBalances[currency], savedBalance = savedBalances?.data?.balances?.[currency] || 0;
             if(currentBalance === savedBalance) continue;
