b616ac6430522db65afbb90080fd7b159d428e36
KaMeHb-UA <marlock@bassteam.ml>
2021-08-19 03:17:09 +0300

    remove useless stuff

--- app/components/core.js
+++ app/components/core.js
@@ -1,23 +1,6 @@
-import { bold } from '../helpers/os/output.js';
-import request from './jsonrpc.js';
-import hooks from './hooks.js';
-
-/**
- * @arg {keyof hooks} name
- * @arg {any} args
- * @return {Promise<[string, any][]>}
- */
-async function callHooks(name, args){
-    // @ts-ignore
-    return Promise.all(hooks[name].map(async ([plugin, url]) => {
-        try{
-            return [plugin, await request(url, name, args)];
-        } catch(e){
-            console.error(`[${bold(plugin)}]: failed to call ${name}: ${e.message}`);
-            return [plugin, undefined];
-        }
-    }));
-}
+import asyncInterval from '../helpers/os/async-interval.js';
 
 export default function(){
+    asyncInterval(async () => {
+    }, 1000);
 }
