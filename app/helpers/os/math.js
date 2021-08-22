772d1fc6f70452648180a5d8cfab3b7a9ba8ba04
KaMeHb-UA <marlock@bassteam.ml>
2021-08-23 01:38:39 +0300

    fix math

--- app/helpers/os/math.js
+++ app/helpers/os/math.js
@@ -12,20 +12,9 @@ function digitsAfterDot(num){
 /**
  * @arg {number} op1
  * @arg {number} op2
- * @arg {(op1: number, op2: number, precision?: number) => number} cb
- */
-function baseOp(op1, op2, cb){
-    const dad1 = digitsAfterDot(op1), dad2 = digitsAfterDot(op2);
-    const targetPrecision = 10 ** (dad1 > dad2 ? dad1 : dad2);
-    return cb(op1 * targetPrecision, op2 * targetPrecision, targetPrecision) / targetPrecision;
-}
-
-/**
- * @arg {number} op1
- * @arg {number} op2
  */
 export function minus(op1, op2){
-    return baseOp(op1, op2, (op1, op2) => op1 - op2)
+    return +(op1 - op2).toFixed(Math.max(digitsAfterDot(op1), digitsAfterDot(op2)));
 }
 
 /**
@@ -33,5 +22,5 @@ export function minus(op1, op2){
  * @arg {number} op2
  */
 export function plus(op1, op2){
-    return baseOp(op1, op2, (op1, op2) => op1 + op2)
+    return +(op1 + op2).toFixed(Math.max(digitsAfterDot(op1), digitsAfterDot(op2)));
 }
