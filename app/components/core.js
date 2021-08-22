2eb3f05c338404602e37843e03d46ed050f7c19d
KaMeHb-UA <marlock@bassteam.ml>
2021-08-23 01:14:06 +0300

    update core with temporary publish and currencyInfo functions

--- app/components/core.js
+++ app/components/core.js
@@ -1,6 +1,33 @@
 import asyncInterval from '../helpers/os/async-interval.js';
+import { add } from './db.js';
+import callHook from './hooks.js';
+import getIcon from './mcc-icons.js';
+import getNewTxs from './txs.js';
 
-export default function(){
-    asyncInterval(async () => {
-    }, 1000);
+async function publish(type, data){
+    await callHook('publish', { type, data });
 }
+
+async function currencyInfo(code){
+    const resp = await callHook('currency_info', { code });
+    for(const provider in resp) return resp[provider];
+}
+
+export default () => asyncInterval(true, async () => {
+    const txs = await getNewTxs();
+    for(const tx of txs){
+        await add('transactions', tx);
+        const currency = await currencyInfo(tx.currency);
+        await publish('new_tx', {
+            provider: tx.provider,
+            icon: getIcon(tx.mcc),
+            currency_prefix: currency.symbol,
+            currency_suffix: currency.symbol ? '' : ` ${tx.currency}`,
+            description: tx.description,
+            amount: tx.amount,
+            comission: tx.comission,
+            cashback: tx.cashback,
+            rest_balance: tx.restBalance,
+        });
+    }
+}, 60000)
